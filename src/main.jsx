28e13ff08bfc7cab67036e6d464912c8f3a5b61a
feliciocavalcante <feliciospfc@gmail.com>
2025-06-23 21:59:47 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login.jsx";
-import TransactionsPage from "./pages/Transactions.jsx";
-import TransactionDetails from "./pages/TransactionDetails.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 import { ToastContainer } from "react-toastify";
 import { TransactionProvider } from "./contexts/TransactionContext.jsx";
 
@@ -14,8 +13,9 @@ createRoot(document.getElementById("root")).render(
       <TransactionProvider>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
-          <Route path="/transactions/:id" element={<TransactionDetails />} />
+          <Route path="/dashboard" element={<DashboardPage />} index />
+          <Route path="*" element={<h1>404 - Not Found</h1>} />
+          
         </Routes>
       </TransactionProvider>
     </BrowserRouter>
